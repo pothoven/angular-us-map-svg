f29f745d0336c75a45aab5c100cf27d75497afb2
Steven Pothoven <steven@pothoven.net>
2025-05-21 14:56:21 -0400

    Include SVG us-map component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
+/**
+ * Updated version of angular4-us-map-svg
+ * https://github.com/bharat20185/angular4-us-map-svg
+ *
+ */
+
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { UsMapComponent } from "./us-map/us-map.component";
 
 @Component({
-    selector: 'app-root',
-    imports: [CommonModule, RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  imports: [CommonModule, UsMapComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'us-map-svg';
+
+  usMapClick($event: any) {
+    console.log($event);
+  }
 }
