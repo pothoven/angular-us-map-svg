58e8574bc864fa0ce69223adc84fb3c70736862b
Steven Pothoven <steven@pothoven.net>
2025-05-22 17:31:54 -0400

    Add app module

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { UsMapModule } from './us-map/us-map.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    UsMapModule
+  ],
+  providers: [],
+})
+export class AppModule { }
